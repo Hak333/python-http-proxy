81611979060276a631c4a7487ce8fd0300fe7eb6
Hak333 <115870989+Hak333@users.noreply.github.com>
2023-03-05 15:49:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import threading, os
 
+PORT = your-port
+
 def vpn():
-	os.system('python -m pproxy -l http://0.0.0.0:1087')
+	if PORT == your-port:
+		print("Please change port in main.py")
+		exit()
+	else:
+		os.system(f'python -m pproxy -l http://0.0.0.0:{PORT}')
+		
 threading.Thread(target=vpn).start()
